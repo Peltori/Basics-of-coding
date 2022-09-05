bf47f521eb3cd2850ed7a9145396580123f56877
Petri Peltomaa <ac7995@student.jamk.fi>
2022-09-05 18:37:08 +0300

    Muutettu koodien kommentointia hieman

--- Lab02/L02_T04.py
+++ Lab02/L02_T04.py
@@ -1,11 +1,12 @@
-# määritetään muuttuja firstname ja age_years, kysytään käyttäjän etunimi sekä syntymävuosi
+''' määritetään muuttuja firstname ja age_years, kysytään käyttäjän etunimi sekä syntymävuosi
+    haetaan import määritteellä tämän hetkinen vuosi
+    määritetään muuttuja now ja tälle muuttujalle kuluva päivämäärä 
+    määritetään muutujalle current_year kuluva vuosi
+'''
 firstname = input("Anna etunimesi: ")
 age_years = int(input("Anna syntymävuotesi: "))
-# haetaan import määritteellä tämän hetkinen vuosi 
 import datetime
-# määritetään muuttuja now ja tälle muuttujalle kuluva päivämäärä
 now = datetime.datetime.now()
-# määritetään muutujalle current_year kuluva vuosi
 current_year = now.year
 # tulostetaan käyttäjän antama nimi ja vähennetään kuluvasta vuodesta käyttäjän antama syntymävuosi ja tulostetaan erotus
 print(f"Hei {firstname}, olet {current_year - age_years} vuotias ")
--- Lab02/L02_T05.py
+++ Lab02/L02_T05.py
@@ -1,5 +1,10 @@
+''' määritetään muuttuja firstname ja kysytään käyttäjän etunimi
+    määritetään muuttuja length ja haetaan merkkijonon pituus funktion len avulla
+    määritetään muuttuja first ja indeksin avulla haetaan merkkijonon ensimmäinen kirjain muuttujasta firstname
+'''
 firstname = input("Anna etunimesi: ")
 length = len(firstname)
 first = firstname[0]
+#tulostetaan muuttujat firstname ja length sekä kerrotaan muuttuja first ja length keskenään ja tulostetaan muuttujien tulo
 print(f"Nimessäsi {firstname} on {length} kirjainta. ")
 print(first * length)
