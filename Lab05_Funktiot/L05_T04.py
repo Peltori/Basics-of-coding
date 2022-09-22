d991c89e43f3f4fbbb9287a53b94d4801f08ca24
Petri Peltomaa <ac7995@student.jamk.fi>
2022-09-22 23:39:24 +0300

    Lisätty labra 5 tehtävä 5

---
+++ Lab05_Funktiot/L05_T04.py
@@ -0,0 +1,16 @@
+'''
+    author: Petri Peltomaa
+    ohjelmoinnin perusteet: Labra 5 tehtävä 4
+
+    funktio laskee ja palauttaa automatkaan kuluneen
+    polttoaineen määrän yhden desimaalin tarkkuudella
+
+'''
+
+
+def get_fuel(km, keskikulutus):
+    litrat = ((km * keskikulutus) / 100)
+    return (f"Polttoainetta kulunut {litrat:.1f} ltr")
+
+print(get_fuel(100, 7.5))
+print(get_fuel(220, 6.9))
---
+++ Lab05_Funktiot/L05_T05.py
@@ -0,0 +1,16 @@
+'''
+    author: Petri Peltomaa
+    ohjelmoinnin perusteet: Labra 5 tehtävä 4
+
+    funktio laskee ja palauttaa automatkaan kuluneen
+    polttoaine kustannuksen 2 desimaalin tarkkuudella
+
+'''
+
+
+def get_fuel(km, keskikulutus, litrahinta):
+    kustannus = ((km * keskikulutus) / 100 * litrahinta)
+    return (f"kustannus {kustannus:.2f} €")
+
+print(get_fuel(100, 7.5, 1.88))
+print(get_fuel(220, 6.9, 1.88))
