6af7fbf08a97e9da907fa5d6dd1a34616c673e3e
Petri Peltomaa <ac7995@student.jamk.fi>
2022-09-29 15:13:22 +0300

    Muokattu koodia

--- Lab06_Kertaustehtävät/L06_T01.py
+++ Lab06_Kertaustehtävät/L06_T01.py
@@ -9,7 +9,6 @@
 # funktion alustus
 
 def showtime(sekunnit):
-    sekunnit = sekunnit % (24 * 3600)
     tunnit = sekunnit // 3600
     sekunnit %= 3600
     minuutit = sekunnit // 60
@@ -17,3 +16,5 @@ def showtime(sekunnit):
     return (f"{tunnit:02d}:{minuutit:02d}:{sekunnit:02d}")
 
 print(showtime(500))
+print(showtime(10000))
+print(showtime(121000))
