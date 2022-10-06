aeead05ef87ff526db50add8ac9d7bfc02f490df
Petri Peltomaa <AC7995@student.jamk.fi>
2022-10-06 13:42:11 +0300

    lisätty labra 7 tehtävä 2 ei valmis

---
+++ Lab07_Funktiot_ja_oliot/L07_T02.py
@@ -0,0 +1,14 @@
+'''
+    author: Petri Peltomaa
+    ohjelmoinnin perusteet: Labra 7 tehtävä 2
+
+    luodaan Human luokka, jolla on kaksi ominaisuutta: name ja age
+    
+
+'''
+
+# luokan määrittely ja luokan ominaisuuksien määrittely
+
+class Human:
+    name = ""
+    age = ""
