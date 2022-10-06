8b3b91c487c8e587fece156d1ff100d52b33908a
Petri Peltomaa <AC7995@student.jamk.fi>
2022-10-06 18:41:18 +0300

    Lisätty labra 7 tehtävä 2

---
+++ Lab07_Luokat_ja_oliot/L07_T02.py
@@ -0,0 +1,30 @@
+'''
+    author: Petri Peltomaa
+    ohjelmoinnin perusteet: Labra 7 tehtävä 2
+
+    luodaan Human luokka, jolla on kaksi ominaisuutta: name ja age
+    
+
+'''
+
+# luokan määrittely ja luokan ominaisuuksien määrittely
+
+class Human():
+    name = ""
+    age = ""
+    def __str__(self):
+        return str(self.name) + ", " + str(self.age) + " "
+
+# luodaan Human-oliot ja asetetaan ominaisuudet
+
+Adam = Human()
+Adam.name = "Nimi: Adam"
+Adam.age = "Ikä: 19"
+
+Eva = Human()
+Eva.name = "Nimi: Eva"
+Eva.age = "Ikä: 18"
+
+
+print(Adam)
+print(Eva)
