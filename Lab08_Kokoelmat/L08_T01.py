47374ade2540f5fa7590c22729c74394f1de172c
Petri Peltomaa <AC7995@student.jamk.fi>
2022-10-13 21:45:31 +0300

    Lisätty isidentidier metodi looppiin

--- Lab08_Kokoelmat/L08_T01.py
+++ Lab08_Kokoelmat/L08_T01.py
@@ -13,16 +13,24 @@
 kaverit = []
 nimet_laskuri = 0
 
+'''
+    käytin tässä "isidentifier()" string metodia, koska se ei hyväksy numeroilla alkavaa stringia
+    mutta hyväksyy kuitenkin numeroita syötteessä muualla kuin alussa, joten tämäkään tapa ei ole täydellinen tarkastamaan
+    onko annettu nimi tekstiä vaiko ei, myöskään kaksiosaiset nimet eivät onnistu
+    koska jos syötteessä on "-" niin se ei ole silloin identifier
+
+'''
 # while loop lisää annetut nimet listaan ja katkaisee loopin kun 10 nimeä on annettu
 
 while True:
     nimet = input("Anna kaverin nimi: ")
-    nimet_laskuri += 1
-    kaverit.append(nimet)
-    if nimet_laskuri == 10:
+    if nimet.isidentifier() == False:
+        print("Et antanut nimeä")
+    elif nimet_laskuri == 10:
         break
     else:
-        continue
+        nimet_laskuri += 1
+        kaverit.append(nimet)
 
 # tulostetaan listan sisältö samassa järjestyksessä kuin nimet on annettu ja tämän jälkeen käänteisessä järjestyksessä
 
