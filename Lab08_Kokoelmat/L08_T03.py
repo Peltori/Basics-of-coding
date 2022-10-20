e913af1dc62205f492f06eb410fcc224c530e3a9
Petri Peltomaa <AC7995@student.jamk.fi>
2022-10-20 20:49:38 +0300

    Koodiin lisätty inputin tarkistus

--- Lab08_Kokoelmat/L08_T03.py
+++ Lab08_Kokoelmat/L08_T03.py
@@ -13,13 +13,25 @@
 arvosanat = []
 
 # while loop lisää annetut arvosanat listaan ja katkaisee loopin kun käyttäjä antaa tyhjän syötteen
+## looppi myös tarkastaa antaako käyttäjä 0:n tai positiivisen kokonaisluvun
+### myöskään negatiivisia kokonaislukuja ei lisätä listaan ja ohjeistetaan käyttäjää antamaan kokonaisluku 0-5 väliltä
+#### tarkistetaan myös antaako käyttäjä desimaalilukuja
+##### loopissa on ongelma että jos numeroita annetaan vähintään yksi ja sen jälkeen annetaan kirjain niin syötettä ei enää kysytä
 
 while True:
     try:
-        arvosana = int(input("Anna kurssin arvosana (1,2,3,4 tai 5): "))
+        arvosana = int(input("Anna kurssin arvosana (0,1,2,3,4 tai 5): "))
+
     except ValueError:
+        if len(arvosanat) == 0:
+            print("Listassa ei ole arvosanoja")
+            continue
         break
+
+    if arvosana < 0 or arvosana > 5:
+        print("Annoitko varmasti kokonaisluvun 0-5 väliltä?")
     else:
+        arvosana >= 0
         arvosanat.append(arvosana)
         continue
 
@@ -29,5 +41,5 @@ keskiarvo = sum(arvosanat) / len(arvosanat)
 
 # tulostetaan arvosanojen määrä ja keskiarvo
 
-print("annettujen arvosanojen määrä: ", len(arvosanat))
+print("Annettujen arvosanojen määrä: ", len(arvosanat))
 print(f"Lukujen keskiarvo on:  {keskiarvo:.1f}")
