2c2ad6c6e515caa76223f0877c43e99671b91f74
Petri Peltomaa <AC7995@student.jamk.fi>
2022-10-21 19:20:13 +0300

    Lisätty labra 9 tehtävä 2

---
+++ Lab09_Tiedostot/L09_T02.py
@@ -0,0 +1,30 @@
+'''
+    author: Petri Peltomaa
+    ohjelmoinnin perusteet: Labra 9 tehtävä 2
+
+    ohjelma lukee aikaisemmasta tehtävästä syötetyt nimet tiedostosta ja näyttää ne konsolilla
+    tämän jälkeen ohjelma lajittelee nimet aakkosjärjestykseen ja näyttää ne kosolilla uudelleen
+
+'''
+
+# luodaan tyhjä lista
+
+nimet_lista = []
+
+# luetaan tiedostosta nimet
+
+file = open(r"tekstitiedostot/testi.txt", "r")
+
+for nimi in file:
+    nimet_lista.append(nimi)
+
+# suljetaan lista
+
+file.close()
+
+print(nimet_lista)
+
+# lajitellaan lista aakkosjärjestykseen ja näytetään lista konsolilla
+
+nimet_lista.sort()
+print(nimet_lista)
