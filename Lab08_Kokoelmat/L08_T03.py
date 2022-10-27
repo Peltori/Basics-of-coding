6d03a75371d139f69aceeebe2b48737adb678b43
Petri Peltomaa <ac7995@student.jamk.fi>
2022-10-27 16:35:25 +0300

    Muokkauksia kommentointiin

--- Lab08_Kokoelmat/L08_T03.py
+++ Lab08_Kokoelmat/L08_T03.py
@@ -13,10 +13,14 @@
 arvosanat = []
 
 # while loop lisää annetut arvosanat listaan ja katkaisee loopin kun käyttäjä antaa tyhjän syötteen
+
 ## looppi myös tarkastaa antaako käyttäjä 0:n tai positiivisen kokonaisluvun
+
 ### myöskään negatiivisia kokonaislukuja ei lisätä listaan ja ohjeistetaan käyttäjää antamaan kokonaisluku 0-5 väliltä
-#### tarkistetaan myös antaako käyttäjä desimaalilukuja
-##### loopissa on ongelma että jos numeroita annetaan vähintään yksi ja sen jälkeen annetaan kirjain niin syötettä ei enää kysytä
+
+#### loopissa on ongelma jos numeroita annetaan vähintään yksi ja sen jälkeen annetaan kirjain niin syötettä ei enää kysytä
+
+##### en saanut tehtävä materiaalien ja w3schools sivun avulla ratkaistua ongelmaa jossa listassa on jo numeroita ja vaikka käyttäjä antaisi tyhjän syötteen sijaan muun kuin integerin niin looppi lopetetaan ja jatketaan ohjelman suorittamista loppuun
 
 while True:
     try:
@@ -25,13 +29,13 @@ while True:
     except ValueError:
         if len(arvosanat) == 0:
             print("Listassa ei ole arvosanoja")
-            continue
-        break
+            continue    
+        else:
+            break
 
     if arvosana < 0 or arvosana > 5:
-        print("Annoitko varmasti kokonaisluvun 0-5 väliltä?")
+        print("Annoitko varmasti kokonaisluvun väliltä 0-5 ?")
     else:
-        arvosana >= 0
         arvosanat.append(arvosana)
         continue
 
@@ -41,5 +45,5 @@ keskiarvo = sum(arvosanat) / len(arvosanat)
 
 # tulostetaan arvosanojen määrä ja keskiarvo
 
-print("Annettujen arvosanojen määrä: ", len(arvosanat))
-print(f"Lukujen keskiarvo on:  {keskiarvo:.1f}")
+print("Annettujen arvosanojen määrä: ", len(arvosanat), sep='')
+print(f"Lukujen keskiarvo on: {keskiarvo:.1f}")
