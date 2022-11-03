21ca2e0d8c6126aeca759d41a2a1cb134558e7a1
Petri Peltomaa <AC7995@student.jamk.fi>
2022-11-03 13:16:47 +0200

    Lisätty poikkeusten käsittely

--- Lab06_Kertaustehtavat/L06_T04.py
+++ Lab06_Kertaustehtavat/L06_T04.py
@@ -16,16 +16,20 @@ pisteet = 0
 # while looppi lisää annetut numerot listaan, josta haetaan funktioiden min() ja max() avulla suurin ja pienin luku ja vähennetään ne kokonaispisteistä
 
 while True:
-    hyppy = int(input("Anna hypyn tyylipisteet välillä 1-20 kiitos: "))
-    pisteet_lista.append(hyppy)
-    pisteet += hyppy
-    hypyt += 1
-    if hypyt == 5:
-        pienin = min(pisteet_lista)
-        suurin = max(pisteet_lista)
-        kokonaispisteet = pisteet - suurin - pienin
-        break
-    else:
-        continue
-
+    try:
+        hyppy = int(input("Anna hypyn tyylipisteet välillä 1-20 kiitos: "))
+        if hyppy >= 1:
+            pisteet_lista.append(hyppy)
+            pisteet += hyppy
+            hypyt += 1
+        elif hypyt == 5:
+            pienin = min(pisteet_lista)
+            suurin = max(pisteet_lista)
+            kokonaispisteet = pisteet - suurin - pienin
+            break
+        else:
+            print("Annoitko positiivisen kokonaisluvun")
+            continue
+    except ValueError:
+        print("Annoitko varmasti kokonaisluvun")
 print(kokonaispisteet)
