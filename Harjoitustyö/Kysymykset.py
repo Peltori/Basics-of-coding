791955040105ee4e174084d30aa9ea5edb8e084f
Petri Peltomaa <ac7995@student.jamk.fi>
2022-12-05 19:34:17 +0200

    Lisätty koodiin kysymyksiä sekä tarkistukset

--- Harjoitustyö/Kysymykset.py
+++ Harjoitustyö/Kysymykset.py
@@ -1,22 +1,25 @@
-kysymys1 = "Kuka kehitti ensimmäisen Linux kernelin?:"
-kysymys2 = "Minä vuonna Linux Kernel 1.0 versio julkaistiin?:"
-kysymys3 = "Kuka alunperin kirjoitti GPL lisenssin?"
-kysymys4 = "Minkä lisenssin alle Linus Torvalds julkaisi Linux lähdekoodin"
+# Kysymysten alustus ja listan luonti
 
-lst_kysymykset = [
-    kysymys1,
-    kysymys2,
-    kysymys3,
-    kysymys4
-]
-
-vastaus1 = {
-    "a" : "Linus Torvalds",
-    "b" : "Linux Torvalds",
-    "c" : "Richard Stallman"
+dict_kysymykset = {
+    "Kuka kehitti ensimmäisen Linux kernelin?": [
+        "Linus Torvalds", "Linux Torvalds", "Richard Stallman"
+    ],
+    "Minä vuonna Linux Kernel 1.0 versio julkaistiin?": [
+        "1994", "1995", "1992"
+    ],
+    "Kuka alunperin kirjoitti GPL lisenssin?": [
+        "Richard Stallman", "James Hacker", "Linus Torvalds"
+    ],
+    "Minkä lisenssin alle Linus Torvalds julkaisi Linux lähdekoodin?": [
+        "GPL", "GLP", "LGPL"
+    ],
+    "Millä komennolla asetat tiedoston/hakemiston omistajakäyttäjän tai ryhmän?": [
+        "chown", "chmod", "usermod"
+    ],
+    "Millä komennolla muutat tiedoston/hakemiston oikeuksia?": [
+        "chmod", "chown", "usermod"
+    ],
+    "Millä komennolla saat luotua kansion Linux käyttöjärjestelmässä?": [
+        "mkdir", "ls dir", "touch dir"
+    ]
 }
-
-lst_vastaukset = []
-
-
-#print(testi)
--- Harjoitustyö/Tietovisa.py
+++ Harjoitustyö/Tietovisa.py
@@ -1,8 +1,11 @@
 '''
     author: Petri Peltomaa
-    ohjelmoinnin perusteet: Harjoitustyö
+    ohjelmoinnin perusteet: Harjoitustyö Tietovisa
 
-    Pelissä kysytään kymmenen kysymystä liittyen linux käyttöjärjestelmään/historiaan
+    Tietovisassa kysytään 5 monivalinta kysymystä liittyen linux käyttöjärjestelmään/historiaan
+    Kysymykset ovat omassa tiedostossaan "Kysymykset.py"
+    Ohjelma arpoo kysymykset satunnaisessa järjestyksessä
+    Vastausksien järjestys on myös satunnainnen
 
 '''
 
@@ -10,17 +13,10 @@
 
 import time
 import sys
+import random
 from Kysymykset import *
 
-# muuttujien alustusta
-
-pisteet = 0
-
-# Listan alustusta
-
-lst_keys = []
-
-# Funktion jolla saadaan tekstin tulostus konsoliin hitaammaksi
+# Funktio jolla saadaan tekstin tulostus konsoliin hitaammaksi
 
 def slowPrint(string, speed=0.05):
     for char in string + '\n':
@@ -28,24 +24,48 @@ def slowPrint(string, speed=0.05):
         sys.stdout.flush()
         time.sleep(speed)
 
+# muuttujien alustusta
+
+pisteet = 0
+kysytyt_kysymykset = 0
+kysymysten_maara_per_visa = 5
+
 # Pelin aloitus viestit
 
-slowPrint("Tervetuloa Linux ässä tietovisaan", 0.1)
+slowPrint("Tervetuloa Linuxässä tietovisaan!!", 0.1)
 
 pelaaja = input("Anna pelaajan nimi kiitos: ")
 
 # Arvotaan kysymykset
 
-print(kysymys1)
+kysymysten_maara = min(kysymysten_maara_per_visa, len(dict_kysymykset))
+kysymykset = random.sample(list(dict_kysymykset.items()), k=kysymysten_maara)
 
-for key in vastaus1.keys():
-    lst_keys.append(key)
+while True:
+    try:
+        for num, (kysymys, vaihtoehdot) in enumerate(kysymykset, start=1):
+            slowPrint(f"\nKysymys {num}:")
+            slowPrint(f"{kysymys}")
+            kysytyt_kysymykset += 1
+            oikea_vastaus = vaihtoehdot[0]
+            arvo_vaihtoehdot = random.sample(vaihtoehdot, k=len(vaihtoehdot))
 
-# lst_keys.sort()
+            for numero, vaihtoehto in enumerate(arvo_vaihtoehdot):
+                print(f"  {numero}) {vaihtoehto}")
 
-for key in lst_keys:
-    value = vastaus1[key]
-    print(key,value)
-    vastaus = input("Anna vastaus käytä vain kirjaimia a, b tai c")
-    if vastaus == "a" or vastaus == "A":
-        print("Vastaus oikein! Sait 5 pistettä")
+            vastaus_numero = int(input(f"\nValintasi? "))
+            vastaus = arvo_vaihtoehdot[vastaus_numero]
+
+            if vastaus == oikea_vastaus:
+                pisteet += 1
+                slowPrint("Oikein!!")
+            elif kysytyt_kysymykset == 5:
+                break
+            else:
+                slowPrint("Väärin! Parempi onni ensi kerralla")
+
+    except IndexError:
+        print("Valitsemaasi numeroa ei löytynyt vastauksien valinnasta")
+
+    except ValueError:
+        print("Vain numerot sallittuja")
